1e3761df5c3a6d814a2a8e2b63aa91c37fca5ae8
Kalpana98 <kalpana360395@gmail.com>
2023-05-21 20:46:29 +0530

    Fixed collapseOnSelect Navbar

--- src/client/sections/header/Header.jsx
+++ src/client/sections/header/Header.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import NavbarComponent1 from "../../components/Navbar1";
-import NavbarComponent2 from "../../components/Navbar2";
+import { Nav, Navbar, Container } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 export function Header() {
 	return (
-		<>
-			<NavbarComponent1 />
-			{/* <NavbarComponent2 /> */}
-		</>
+		<Navbar sticky="top" bg="dark" variant="dark" expand="md" collapseOnSelect>
+			<Container>
+				<Navbar.Brand as={Link} to="/" className="sharda">𑆯𑆳𑆫𑆢𑆳𑆥𑆵𑆜𑆁</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse>
+					<Nav className="me-auto">
+						<Nav.Link eventKey="1" as={Link} to="/">Homepage</Nav.Link>
+						<Nav.Link eventKey="2" as={Link} to="/lessons">Lessons</Nav.Link>
+						<Nav.Link eventKey="3" as={Link} to="/about-us">About Us</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
 	);
 }
